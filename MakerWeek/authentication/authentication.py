26e96f6ceef5f6d140307c004314a09ed66eeab6
Ho Tuan Kiet <tuankiet65@gmail.com>
2016-06-03 21:55:24 +0700

    Implement basic password reset system (don't even have mail confirmation, but I don't really care)

--- MakerWeek/authentication/authentication.py
+++ MakerWeek/authentication/authentication.py
@@ -1,29 +1,33 @@
-from flask import Blueprint, render_template, request, redirect, session, g
+from flask import Blueprint, render_template, request, redirect, session, g, url_for
 from MakerWeek.authentication import user
+from MakerWeek.mail import mail
+
 authentication = Blueprint("authentication", __name__, url_prefix="")
+
 
 def isLoggedIn():
     return getattr(g, "user", None) is not None
+
 
 @authentication.route("/login", methods=["GET"])
 def loginPage():
     if isLoggedIn():
         return redirect("/")
-    return render_template("login.html", user=g.user)
+    return render_template("authentication/login.html")
 
 
 @authentication.route("/login", methods=["POST"])
 def login():
     if isLoggedIn():
-        return redirect("/", code=200)
+        return redirect("/")
     username = request.form['username']
     password = request.form['password']
     try:
-        cookie=user.login(username, password)
+        cookie = user.login(username, password)
     except user.LoginFailed:
         return redirect("/login?failure=1")
     else:
-        session['tokenKey'], session['tokenHash']=cookie
+        session['tokenKey'], session['tokenHash'] = cookie
     return redirect("/")
 
 
@@ -31,7 +35,7 @@ def login():
 def registerPage():
     if isLoggedIn():
         return redirect("/")
-    return render_template("register.html", user=g.user)
+    return render_template("authentication/register.html")
 
 
 @authentication.route("/register", methods=["POST"])
@@ -41,11 +45,30 @@ def register():
     print(request.form)
     username = request.form['username']
     password = request.form['password']
-    email=request.form['email']
+    email = request.form['email']
     user.createNewUser(username, password, email)
-    return redirect("/login?created=1", code=200)
+    return redirect("/login?created=1")
+
 
 @authentication.route("/signout")
 def signOut():
     session.clear()
     return redirect("/")
+
+
+@authentication.route("/forgot", methods=["GET"])
+def forgotPage():
+    if isLoggedIn():
+        return redirect("/")
+    return render_template("authentication/forgot.html")
+
+
+@authentication.route("/forgot", methods=["POST"])
+def resetPassword():
+    email = request.form['email']
+    try:
+        newPassword = user.resetPassword(email)
+    except user.UserNotFound:
+        return redirect("/forgot?failure=1")
+    mail.sendEmail(email, "Your new password", "Your new password is {}".format(newPassword))
+    return redirect("/login?reset=1")
--- MakerWeek/authentication/user.py
+++ MakerWeek/authentication/user.py
@@ -1,17 +1,20 @@
 from MakerWeek.database import getDB
 from MakerWeek.authentication import loginToken
+import string
+import random
 import bcrypt
+
 
 class User:
     def __init__(self, userID):
         with getDB() as cursor:
-            cursor.execute("SELECT * FROM user WHERE id=?", (userID, ))
-            row=cursor.fetchone()
+            cursor.execute("SELECT * FROM user WHERE id=?", (userID,))
+            row = cursor.fetchone()
             if row is None:
                 raise UserNotFound
-            self.id=row['id']
-            self.username=row['username']
-            self.email=row['email']
+            self.id = row['id']
+            self.username = row['username']
+            self.email = row['email']
 
 
 def _createTable():
@@ -23,32 +26,55 @@ def _createTable():
                             email VARCHAR NOT NULL UNIQUE
                         )""")
 
+
 def _getHashesPassword(username):
     with getDB() as cursor:
-        cursor.execute("SELECT id, password FROM user WHERE username=?", (username, ))
-        row=cursor.fetchone()
+        cursor.execute("SELECT id, password FROM user WHERE username=?", (username,))
+        row = cursor.fetchone()
         if row is None:
             raise UserNotFound
         return (row['id'], row['password'])
 
+
 def login(username, password):
     try:
-        userID, hash=_getHashesPassword(username)
+        userID, hash = _getHashesPassword(username)
     except UserNotFound:
         raise LoginFailed
-    password=password.encode("utf-8")
-    if bcrypt.hashpw(password, hash)!=hash:
+    password = password.encode("utf-8")
+    if bcrypt.hashpw(password, hash) != hash:
         raise LoginFailed
     return loginToken.create(userID)
 
+
 def createNewUser(username, password, email):
-    password=password.encode("utf-8")
-    password=bcrypt.hashpw(password, bcrypt.gensalt())
+    password = password.encode("utf-8")
+    password = bcrypt.hashpw(password, bcrypt.gensalt())
     with getDB() as cursor:
         cursor.execute("INSERT INTO user(username, password, email) VALUES (?, ?, ?)", (username, password, email))
 
+
+def resetPassword(email):
+    with getDB() as cursor:
+        cursor.execute("SELECT * FROM user WHERE email=?", (email,))
+        result = cursor.fetchone()
+    if result is None:
+        raise UserNotFound
+    newPassword=_generateRandomPassword(20)
+    newPasswordHash=bcrypt.hashpw(newPassword.encode("utf-8"), bcrypt.gensalt())
+    with getDB() as cursor:
+        cursor.execute("UPDATE user SET password=? WHERE email=?", (newPasswordHash, email))
+    return newPassword
+
+def _generateRandomPassword(length):
+    rand = random.SystemRandom()
+    letters = string.ascii_letters + string.digits
+    return ''.join(rand.choice(letters) for _ in range(length))
+
+
 class LoginFailed(Exception):
     pass
+
 
 class UserNotFound(Exception):
     pass
